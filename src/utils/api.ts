59bddb37265b66d6778f939777bfb4254205919c
GagaKor <wexdaq554@gmail.com>
2022-08-30 22:29:48 +0900

    commit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,26 +1,42 @@
-import axios from "axios";
+import axios from 'axios';
+
+interface UserCareteDTO {
+  username: string;
+  password: string;
+}
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001",
-    timeout: 60,
+  baseURL: 'http://localhost:3001',
+  timeout: 60,
 });
 
 const getReq = async (url: string, params?: any) => {
-    try {
-        return await instance.get(url, params);
-    } catch (err) {
-        throw new Error();
-    }
+  try {
+    return await instance.get(url, params);
+  } catch (err) {
+    throw new Error();
+  }
+};
+
+const postReq = async (url: string, data?: any) => {
+  try {
+    return await instance.post(url, data);
+  } catch (err) {
+    throw new Error();
+  }
 };
 
 export default {
-    async main() {
-        return await getReq("/");
-    },
-    async getBoards() {
-        return await getReq("/boards");
-    },
-    async getBoard(id: string) {
-        return await getReq(`/boards/${id}`);
-    },
+  async main() {
+    return await getReq('/');
+  },
+  async getBoards() {
+    return await getReq('/boards');
+  },
+  async getBoard(id: string) {
+    return await getReq(`/boards/${id}`);
+  },
+  async authSignIn(data: UserCareteDTO) {
+    return await postReq('/auth/signin', data);
+  },
 };
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,14 +1,43 @@
+import React, { ChangeEventHandler, EventHandler, useEffect, useState } from 'react';
+import api from '../utils/api';
+
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleInputPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onClickLogin = async () => {
+    const data = {
+      username,
+      password,
+    };
+    console.log(data);
+
+    const res = await api.authSignIn({ username, password });
+    console.log(res);
+  };
+
   return (
-    <>
-      <h1>Login</h1>
-      <div>
-        <input type="text" />
+    <div className="form-inner">
+      <h2>Login</h2>
+      {/* ERROR! */}
+      <div className="form-group">
+        <label htmlFor="username">Name</label>
+        <input type="text" name="username" id="username" onChange={handleInputUsername} />
       </div>
-      <div>
-        <input type="text" />
+      <div className="form-group">
+        <label htmlFor="password">password</label>
+        <input type="password" name="password" id="password" onChange={handleInputPassword} />
       </div>
-    </>
+      <button onClick={onClickLogin}>Login</button>
+    </div>
   );
 };
 
