ad3821922c96b8eb987c499384e659adf79af3ae
KwonHyeokJoo <gagaKor@gmail.com>
2022-11-29 16:18:00 +0900

    서버 https 변경

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://api.gagakor.xyz' : 'http://localhost:8080',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://api.gagakor.xyz' : 'http://localhost:8080',
   timeout: 3600,
 });
 
