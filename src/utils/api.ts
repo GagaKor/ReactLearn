c2ead94ce6c029ce5959b90173c2b0281677d994
GagaKor <wexdaq554@gmail.com>
2023-01-07 00:46:12 +0900

    build

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://api.gagakor.xyz' : 'http://localhost:8080',
+  baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : 'http://localhost:8080',
   timeout: 3600,
 });
 
