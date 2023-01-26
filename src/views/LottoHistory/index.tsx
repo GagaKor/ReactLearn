ea8f0bb04585325d585cd2c939e9aacb36d9ce70
KwonHyeokJoo <gagaKor@gmail.com>
2023-01-26 17:35:52 +0900

    css작업

--- src/views/LottoHistory/index.tsx
+++ src/views/LottoHistory/index.tsx
@@ -1,7 +1,44 @@
 import './styles.scss';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LottoHisotry = () => {
-  return <div>history</div>;
+  const [testArr, setTest] = useState<number[][]>([
+    [1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7],
+    [2, 3, 4, 5, 6, 7],
+  ]);
+
+  return (
+    <div className="lottoHistory-container">
+      <div className="lottoHistory-title">Lotto History</div>
+      <div className="lottoHistory-select-box">
+        <select className="lottoHistory-select">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+      <div className="lottoHistory-box">
+        {testArr && testArr.length > 0 ? (
+          testArr.map((num, idx) => {
+            return (
+              <div key={idx}>
+                {num.map((v, idx2) => {
+                  return <span key={idx2}>{v}</span>;
+                })}
+              </div>
+            );
+          })
+        ) : (
+          <span>데이터가 없습니다.</span>
+        )}
+      </div>
+      <Link to={'/'} className="text-decoration-none">
+        <div className="submit-container">Home</div>
+      </Link>
+    </div>
+  );
 };
 
 export default LottoHisotry;
