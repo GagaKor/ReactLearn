5bfde4b87a67ea36724b8ffcc0737d2a43f794d4
gagakor <wexdaq554@gmail.com>
2023-07-17 17:39:35 +0900

    axios timeout 60s

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : 'http://localhost:8080',
-  timeout: 3600,
+  timeout: 1000 * 60,
   withCredentials: true,
 });
 
